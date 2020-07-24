03e3e009f364439f617278d1af3c46e0a71e3932
JL Bautista <jl.bautistac@gmail.com>
2020-07-24 13:41:30 -0500

    Agregar reglas a .gitignore para pycache

--- users/views.py
+++ users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
+from django.shortcuts import redirect, render
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(request, 'recipes:index')
+
+    return render(request, 'users/login.html')
