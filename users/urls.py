cf7ae9683a49433b441f0e2abd77633a1211d6e2
JL Bautista <jl.bautistac@gmail.com>
2020-07-24 15:40:55 -0500

    Agregar ruta y método para logout

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-    path(route='login/', view=views.login_view, name='login')
+    path(route='login/', view=views.login_view, name='login'),
+    path(route='logout/', view=views.logout_view, name='logout'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
@@ -10,6 +11,13 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect(request, 'recipes:index')
+            return redirect('recipes:index')
+        else:
+            return render(request, 'users/login.html', {'errors': 'El usuario y/o la contraseña no son correctos'})
 
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect ('users:login')
