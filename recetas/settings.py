cd6b299f8a9808e487c963dce26e15121f58367e
JL Bautista <jl.bautistac@gmail.com>
2020-07-24 15:38:17 -0500

    Agregar variable LOGIN_URL en settings

--- recetas/settings.py
+++ recetas/settings.py
@@ -127,3 +127,5 @@ STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
+
+LOGIN_URL = 'users:login'
