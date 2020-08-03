d38de2c530f4050a4fd1d74940f85b92d38f09f2
Jesus_Santos35 <chuy_santos35@hotmail.com>
2020-08-03 18:46:21 -0500

    ver imagenes

--- recetas/urls.py
+++ recetas/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users.views import login_view
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', view=login_view, name='index'),
     path('usuarios/', include(('users.urls', 'users'), namespace='users')),
     path('recetas/', include(('recipes.urls', 'recipes'), namespace='recipes')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
